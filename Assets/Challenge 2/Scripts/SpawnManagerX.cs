be3e6792b3c05ee0224ce4d709759a1cf6d7e444
faruking <faruk.abdulganiyu@gmail.com>
2022-09-07 01:35:03 +0100

    bugs fixed
    The game is now working as intended with no bugs in sight.

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,22 +11,22 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(3,5));
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        int ballsIndex = Random.Range(0,ballPrefabs.Length);
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballsIndex], spawnPos, ballPrefabs[ballsIndex].transform.rotation);
     }
 
 }
